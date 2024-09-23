3b0d5e0423a82c4500d946fb335c7aacf2578683
Muhammad Ali <159954209+MuhammadAliShaikh0@users.noreply.github.com>
2024-09-23 01:23:03 -0700

    Update Firebase.jsx

--- src/Config/Firebase.jsx
+++ src/Config/Firebase.jsx
@@ -8,13 +8,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCJZoYUGsse-4bkmaz2IrZVYGcpUdfnHME",
-  authDomain: "localstorage-195f6.firebaseapp.com",
-  databaseURL: "https://localstorage-195f6-default-rtdb.firebaseio.com",
-  projectId: "localstorage-195f6",
-  storageBucket: "localstorage-195f6.appspot.com",
-  messagingSenderId: "766253277685",
-  appId: "1:766253277685:web:90d31e911c28b220b4c0bc"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
